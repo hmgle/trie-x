9fed493f40e7bac6391401fd57a0cf4c23f9c83d
mingang.he <dustgle@gmail.com>
2019-01-11 23:46:34 +0800

    Update erlang trie demo

--- erlang/trie/src/trie_demo.erl
+++ erlang/trie/src/trie_demo.erl
@@ -8,14 +8,27 @@
 %%====================================================================
 
 %% escript Entry point
-main(_) ->
-    T = trie:insert(trie:new(), "abc", 1),
-    T2 = trie:insert(T, "acd", 2),
-    io:format("~p~n", [trie:lookup(T2, "acd")]),
-    io:format("~p~n", [trie:traversal(T2)]),
-    io:format("~p~n", [trie:expand(T2, "a", 1)]),
+main(Filename) ->
+    case file:open(Filename, [read]) of
+        {ok, File} ->
+            T = file2trie(File, trie:new()),
+            Prefix_tr = trie:expand(T, "tree", 20),
+            io:format("tr...: ~p~n", [Prefix_tr]);
+        {error, Reason} ->
+            io:format("open ~p error: ~p~n", [Filename, Reason])
+    end,
     erlang:halt(0).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+file2trie(File, T) ->
+    case file:read_line(File) of
+        {ok, Line} ->
+            T2 = trie:insert(T, string:trim(Line), 1),
+            file2trie(File, T2);
+        _Eof ->
+            T
+    end.
+
