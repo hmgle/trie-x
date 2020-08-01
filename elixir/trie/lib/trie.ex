e22817d47ec5d9f5bcb1a7a30bd01efe1ac7eb1e
mingang.he <dustgle@gmail.com>
2020-08-01 23:08:38 +0800

    Init elixir

---
+++ elixir/trie/lib/trie.ex
@@ -0,0 +1,18 @@
+defmodule Trie do
+  @moduledoc """
+  Documentation for `Trie`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Trie.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/trie/test/trie_test.exs
@@ -0,0 +1,8 @@
+defmodule TrieTest do
+  use ExUnit.Case
+  doctest Trie
+
+  test "greets the world" do
+    assert Trie.hello() == :world
+  end
+end
