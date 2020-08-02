57ea349ea9f3bad781c13355b266eca96bd6b659
mingang.he <dustgle@gmail.com>
2020-08-02 22:00:32 +0800

    Update elixir

--- elixir/trie/lib/trie.ex
+++ elixir/trie/lib/trie.ex
@@ -12,7 +12,31 @@ defmodule Trie do
       :world
 
   """
-  def hello do
-    :world
+  defstruct children: %{}, data: nil
+
+  def new do
+    %Trie{}
+  end
+
+  def insert(trie, [], data) do
+    Map.put(trie, :data, data)
+  end
+  def insert(trie, [k1 | kpai], data) do
+    child = trie[:children]
+    case Map.has_key?(child, k1) do
+      true -> Map.put(trie, k1, insert(child[k1], kpai, data))
+      false -> Map.put(trie, k1, insert(new(), kpai, data))
+    end
+  end
+
+  def lookup(trie, []) do
+    trie[:data]
+  end
+  def lookup(trie, [k1 | kpai]) do
+    child = trie[:children]
+    case Map.has_key?(child, k1) do
+      true -> lookup(child[k1], kpai)
+      false -> :undefined
+    end
   end
 end
