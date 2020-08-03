1a68d5532ad716f392d73ebb351c510f79804670
mingang.he <dustgle@gmail.com>
2020-08-03 23:41:49 +0800

    Update

--- elixir/trie/lib/trie.ex
+++ elixir/trie/lib/trie.ex
@@ -55,7 +55,35 @@ defmodule Trie do
         _ -> kvs2 = [{path, data} | kvs]
       end
         children = trie[:children]
-        ## TODO
+        List.foldl(Map.keys(children), kvs2, fn child, ret ->
+          ret ++ traversal(children[child], path ++ [child], '')
+        end)
+    end
+  end
+
+  def traversal_limit(trie, max_cnt) do
+  end
+  defp traversal_limit(_, _, kvs, max_cnt, cnt) when cnt >= max_cnt do
+    {kvs, cnt}
+  end
+  defp traversal_limit(trie, path, kvs, max_cnt, cnt) do
+    data = trie[data]
+    case map_size(trie[children]) do
+      0 ->
+        case data do
+          nil -> {kvs, cnt}
+          _ -> {kvs ++ [{path, data}], cnt + 1}
+        end
+      _ ->
+        case data do
+          nil -> cnt2 = cnt; kvs2 = kvs
+          _ -> cnt2 = cnt + 1; kvs2 = [{path, data} | kvs]
+        end
+        children = trie[children]
+        List.foldl(Map.keys(children), {kvs2, cnt2}, fn child, ret ->
+          next = traversal_limit(children[child], path ++ [child], '', max_cnt, :erlang.element(2, ret))
+          {:erlang.element(1, ret) ++ :erlang.element(1, next), :erlang.element(2, next)}
+        end)
     end
   end
 end
